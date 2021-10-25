850b0c275f7ed9d471f18cf26d7123224ffabc6b
mfurkan-altun <furkan.altun@yemeksepeti.com>
2021-10-25 15:50:14 +0300

    GET method and default routings added

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/mfurkan-altun/fiber-todo/routes"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
+
+	// setup routes
+	setupRoutes(app) // new
+
 	// give response when at /
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"success":  true,
-			"message": "You are at the endpoint 😉",
+			"message": "You are at the endpoint 😉 (localhost:8000)",
 		})
 	})
 
@@ -23,4 +28,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func setupRoutes(app *fiber.App) {
+	// give response when at /
+	app.Get("/furk", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success":  true,
+			"message": "You are at the endpoint 😉 (localhost:8000/furk)",
+		})
+	})
+
+	// api group
+	api := app.Group("/api")
+
+	// give response when at /api
+	api.Get("", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the api endpoint 😉 (localhost:8000/api)",
+		})
+	})
+
+	// connect todo routes
+	routes.TodoRoute(api.Group("/todos"))
 }
